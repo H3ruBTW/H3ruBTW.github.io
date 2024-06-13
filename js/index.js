8db15bdeb3cf63140e2ec46fd521951a012312ff
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-06-13 18:50:19 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,25 +1,28 @@
 const userAgent = navigator.userAgent || window.opera;
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+console.log("User Agent: ", userAgent);
 
 if (/android/i.test(userAgent)) {
     if (/mobile/i.test(userAgent)) {
+        console.log("Redirecting to tel.html");
         window.location.href = 'tel.html';
     } else {
+        console.log("Redirecting to tab.html");
         window.location.href = 'tab.html';
     }
-} else if (/iPad|iPhone|iPod/i.test(userAgent) && isTouchDevice) {
+} else if (/iPhone|iPad|iPod/i.test(userAgent) && isTouchDevice) {
     if (/iPad/i.test(userAgent)) {
+        console.log("Redirecting to tab.html");
         window.location.href = 'tab.html';
     } else {
+        console.log("Redirecting to tel.html");
         window.location.href = 'tel.html';
     }
 } else if (isTouchDevice && /Macintosh/i.test(userAgent) && navigator.maxTouchPoints > 1) {
-    // Gestisce iPad che si presentano come Mac
+    // Questo controllo è per iPadOS 13 e successivi
+    console.log("Redirecting to tab.html (iPad detected as Mac)");
     window.location.href = 'tab.html';
 } else {
+    console.log("Redirecting to PCmedio.html");
     window.location.href = 'PCmedio.html';
 }
-
-
-
-
-
