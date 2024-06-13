265d3777be0238b72f67abc0c1485628c6883c5b
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-06-13 18:44:58 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 const userAgent = navigator.userAgent || window.opera;
 
 if (/android/i.test(userAgent)) {
-    if(/mobile/.test(userAgent))
-        window.location.href = 'tel.html' 
-    else
-        window.location.href = "tab.html"
-} else if(/iPhone|iPad/.test(userAgent) && !window.MSStream){
-    if(/iPad/.test(userAgent))
-        window.location.href = "tab.html"
-    else
-        window.location.href = 'tel.html'
+    if (/mobile/i.test(userAgent)) {
+        window.location.href = 'tel.html';
+    } else {
+        window.location.href = 'tab.html';
+    }
+} else if (/iPad|iPhone|iPod/i.test(userAgent) && isTouchDevice) {
+    if (/iPad/i.test(userAgent)) {
+        window.location.href = 'tab.html';
+    } else {
+        window.location.href = 'tel.html';
+    }
+} else if (isTouchDevice && /Macintosh/i.test(userAgent) && navigator.maxTouchPoints > 1) {
+    // Gestisce iPad che si presentano come Mac
+    window.location.href = 'tab.html';
 } else {
-    window.location.href = 'PCmedio.html'
+    window.location.href = 'PCmedio.html';
 }
 
 
